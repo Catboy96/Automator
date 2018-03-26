42bed6cb0a8e13f3ee47d66df6a383ac02cd8c3e
Ralf Ren <me@ralf.ren>
2018-03-26 22:16:04 +0800

    UI Done.

--- Automator/Automator/MainWindow.xaml.vb
+++ Automator/Automator/MainWindow.xaml.vb
@@ -1,3 +1,26 @@
 ﻿Class MainWindow
 
+    Private Sub btnNewFile_Click(sender As Object, e As RoutedEventArgs)
+        If txtCode.Text <> "" Then
+            If MessageBox.Show("Create a new file? All unsaved changes will be lost.", "", MessageBoxButton.YesNo, MessageBoxImage.Question) = MessageBoxResult.Yes Then
+                txtCode.Clear()
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub btnInput_Click(sender As Object, e As RoutedEventArgs)
+        If tgbReturn.IsChecked = True Then
+            txtCode.AppendText("DigiKeyboard.println("""");")
+        Else
+            txtCode.AppendText("DigiKeyboard.print("""");")
+        End If
+    End Sub
+
+    Private Sub btnDelay_Click(sender As Object, e As RoutedEventArgs)
+        txtCode.AppendText("DigiKeyboard.delay();")
+    End Sub
 End Class
