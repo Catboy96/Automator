e3ee47cb471c3c5186b38ce17f4df70a65e0c6c5
Ralf Ren <me@ralf.ren>
2018-03-26 20:11:11 +0800

    Let's totally rebuild this app!

---
+++ Automator/Automator/Application.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class Application
+
+    ' 应用程序级事件(例如 Startup、Exit 和 DispatcherUnhandledException)
+    ' 可以在此文件中进行处理。
+
+End Class
--- Automator/Automator/My Project/AssemblyInfo.vb
+++ Automator/Automator/My Project/AssemblyInfo.vb
@@ -1,6 +1,9 @@
 ﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
 
 ' 有关程序集的一般信息由以下
 ' 控制。更改这些特性值可修改
@@ -9,16 +12,37 @@ Imports System.Runtime.InteropServices
 '查看程序集特性的值
 
 <Assembly: AssemblyTitle("Automator")>
-<Assembly: AssemblyDescription("Digispark HID Keyboard Automator")>
-<Assembly: AssemblyCompany("CYROFORCE.NET")>
-<Assembly: AssemblyProduct("Digispark HID Keyboard Automator")>
-<Assembly: AssemblyCopyright("©2017 CYROFORCE.NET")>
+<Assembly: AssemblyDescription("Digispark™ HID Keyboard Automator")>
+<Assembly: AssemblyCompany("CYRO4S")>
+<Assembly: AssemblyProduct("Digispark™ HID Keyboard Automator")>
+<Assembly: AssemblyCopyright("©2017-2018 CYRO4S. All rights reserved.")>
 <Assembly: AssemblyTrademark("Automator")>
+<Assembly: ComVisible(false)>
 
-<Assembly: ComVisible(False)>
+'若要开始生成可本地化的应用程序，请设置
+'在您的 .vbproj 文件中的 <PropertyGroup> 内设置 <UICulture>CultureYouAreCodingWith</UICulture>。
+'例如，如果您在源文件中使用的是美国英语，
+'请将 <UICulture> 设置为“en-US”。  然后取消下面对
+'NeutralResourceLanguage 特性的注释。  更新下面行中的“en-US”
+'以与项目文件中的 UICulture 设置匹配。
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)>
+
+
+'ThemeInfo 特性说明在何处可以找到任何特定于主题的和一般性的资源词典。
+'第一个参数: 特定于主题的资源词典的位置
+'(未在页面中找到资源时使用，
+'或应用程序资源字典中找到时使用)
+
+'第二个参数: 一般性资源词典的位置
+'(未在页面中找到资源时使用，
+'资源词典中找到资源时使用)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
 
 '如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-<Assembly: Guid("12fb8b4d-beea-4189-beaf-d6a556b81f34")>
+<Assembly: Guid("b40fb8d1-ac74-44b8-8611-b1be342476d1")>
 
 ' 程序集的版本信息由下列四个值组成: 
 '
@@ -27,9 +51,9 @@ Imports System.Runtime.InteropServices
 '      生成号
 '      修订号
 '
-'可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+' 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 ' 方法是按如下所示使用“*”: :
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.1.0")>
-<Assembly: AssemblyFileVersion("1.0.1.0")>
+<Assembly: AssemblyVersion("2.0.0.0")>
+<Assembly: AssemblyFileVersion("2.0.0.0")>
